0ca8aac0f1c07bf38032f2093356f18f383a2b66
StationToStation <anastasgarbuz@gmail.com>
2019-07-29 16:22:35 +0300

    initial commit

---
+++ src/extensions/viewCount/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IViewCountApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'ViewCountApplicationCustomizerStrings' {
+  const strings: IViewCountApplicationCustomizerStrings;
+  export = strings;
+}
